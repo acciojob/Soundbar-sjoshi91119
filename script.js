f27ffd8b4120dd4a284390984466b095b04eeb5c
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-04-20 03:39:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+		function playSound() {
+			var audio = document.getElementById("audio");
+			audio.play();
+		  }
+		function stopSound(){
+			var audio = document.getElementById("audio");
+			audio.pause();
+		}
